bcd218a6638288e9e25eda966dff9721c5dbb133
alishak82 <alisha82@gmail.com>
2015-10-26 15:45:02 -0400

    project description overlay and links working

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,26 @@ $(document).ready(function() {
 	$("nav a").click(function() {
 		var href = $(this).attr("href");
 		$.scrollTo(href, 700);
+	});
+
+	$(".footer-nav a").click(function() {
+		var href = $(this).attr("href");
+		$.scrollTo(href, 700);
+	});
+
+
+	$('.projects li').mouseenter(function() {
+		var descript = $(this).data('descript');
+		var link = $(this).data('href');
+
+		var html = '<div class="descript">';
+				html +='<p style="font-size: 12px; font-family: Arial; color: black;" class="descript-text">' + descript + '</p>';
+				html += '<center><a href="' + link + '">Click to Launch</a></center>';
+				html += '</div>';
+
+		$('.descript').remove();
+		$(this).append(html).mouseleave(function() {
+			$('.descript').remove();
+		});
 	})
-})
+});
